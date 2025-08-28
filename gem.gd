e27a70b3433691ee94ef2e52a66c327358ec6c26
Will Roberts <willroberts3@protonmail.com>
2025-08-28 00:10:43 -0400

    Tweaks color setting

--- gem.gd
+++ gem.gd
@@ -4,14 +4,15 @@ var color: String
 var colors: Array = ['blue', 'green', 'purple', 'red', 'yellow']
 
 func _init() -> void:
-	color = colors[randi() % colors.size()]
+	pass
 
 func _ready() -> void:
-	var tex = load("res://assets/png/diamonds/element_%s_diamond_glossy.png" % color)
-	$Sprite2D.set_texture(tex)
+	set_color(colors[randi() % colors.size()])
 
 func get_color() -> String:
 	return color
 
 func set_color(c: String) -> void:
 	color = c
+	var tex = load("res://assets/png/diamonds/element_%s_diamond_glossy.png" % c)
+	$Sprite2D.set_texture(tex)
