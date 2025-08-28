ed0a68d97100615d43921e40984d2a57d2642884
Will Roberts <willroberts3@protonmail.com>
2025-08-28 00:21:06 -0400

    Preloads textures

--- gem.gd
+++ gem.gd
@@ -2,9 +2,13 @@ extends Node2D
 
 var color: String
 var colors: Array = ['blue', 'green', 'purple', 'red', 'yellow']
-
-func _init() -> void:
-	pass
+var textures: Dictionary = {
+	'blue': preload("res://assets/png/diamonds/element_blue_diamond_glossy.png"),
+	'green': preload("res://assets/png/diamonds/element_green_diamond_glossy.png"),
+	'purple': preload("res://assets/png/diamonds/element_purple_diamond_glossy.png"),
+	'red': preload("res://assets/png/diamonds/element_red_diamond_glossy.png"),
+	'yellow': preload("res://assets/png/diamonds/element_yellow_diamond_glossy.png"),
+}
 
 func _ready() -> void:
 	set_color(colors[randi() % colors.size()])
@@ -13,6 +17,8 @@ func get_color() -> String:
 	return color
 
 func set_color(c: String) -> void:
+	if c not in textures:
+		push_error('invalid color: %s' % c)
+		c = 'blue'
 	color = c
-	var tex = load("res://assets/png/diamonds/element_%s_diamond_glossy.png" % c)
-	$Sprite2D.set_texture(tex)
+	$Sprite2D.set_texture(textures[c])
